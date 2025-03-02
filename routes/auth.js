f06bfedfa9652959bdd72d381a3275f867dff25e
natnichaxxx <natnib.xx@gmail.com>
2025-03-02 14:14:52 +0700

    delete expiresIn

--- routes/auth.js
+++ routes/auth.js
@@ -45,7 +45,7 @@ router.post('/login', async (req, res) => {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    const token = jwt.sign({ userId: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user._id }, 'your_jwt_secret');
 
     res.json({ token });
   } catch (error) {
